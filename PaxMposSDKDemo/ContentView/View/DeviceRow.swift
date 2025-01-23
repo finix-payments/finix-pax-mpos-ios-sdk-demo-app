ab09f148c98416503fd7f25b1d4b8512e52ff4ef
Tom Nguyen <tom.nguyen@finix.com>
2025-01-22 17:44:35 -0800

    feat: add the demo app

---
+++ PaxMposSDKDemo/ContentView/View/DeviceRow.swift
@@ -0,0 +1,29 @@
+//
+//  DeviceRow.swift
+//  PaxMposSDKDemo
+//
+//  Created by Tom Nguyen on 1/13/25.
+//
+
+import SwiftUI
+
+struct DeviceRow: View {
+    let device: Device
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            if !device.name.trim().isEmpty {
+                Text(device.name)
+                    .font(Constants.bodyFont)
+                    .fontWeight(.bold)
+            }
+            Text(device.id)
+                .font(Constants.footnoteFont)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+#Preview {
+    DeviceRow(device: .init(id: "123", name: "Device 1"))
+}
---
+++ PaxMposSDKDemo/Helper/View+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  View+Ext.swift
+//  PaxMposSDKDemo
+//
+//  Created by Tom Nguyen on 1/13/25.
+//
+
+import SwiftUI
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder),
+                                        to: nil,
+                                        from: nil,
+                                        for: nil)
+    }
+}
